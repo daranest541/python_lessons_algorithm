0d8efcee2c4d0e506c3e7095f92c78d8fd39d8bc
daranest541 <rjkmzy@gmail.com>
2018-11-01 05:06:03 +0300

    empty

--- lesson_2/task_4.py
+++ lesson_2/task_4.py
@@ -1,19 +1,3 @@
 # Задача 4. Найти сумму n элементов(1 + -0,5 + 0,25..)
 
-n = int(input('Введите n: '))
 
-if n <= 1:
-    print('n должно быть больше 1')
-    exit()
-
-op, i, summ = 1, 0, 1
-while n - 1 > i:
-    if i % 2 == 0:
-        op = 0 - op / 2
-    else:
-        op = abs(op / 2)
-    # print(f'iter {i + 1} op {op}')
-    summ = summ + op
-    i += 1
-
-print(f'сумма ряда равна {summ}')
