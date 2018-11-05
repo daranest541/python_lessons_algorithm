12b51790898312cc51dc6443a4a4568576ba3416
daranest541 <rjkmzy@gmail.com>
2018-11-05 18:19:39 +0300

    3 correct!

--- lesson_3/task_3.py
+++ lesson_3/task_3.py
@@ -2,11 +2,12 @@
 import random
 
 SIZE = 10
-array = [random.randint(0, SIZE * SIZE) for _ in range(SIZE)]
+# array = [random.randint(0, SIZE * SIZE) for _ in range(SIZE)]
+array = [-818, -339, -286, -922, -623, -445, -850, -459, -908, -832]
 print(array)
 max_num_idx = 0
 min_num_idx = 0
-max_num = 0
+max_num = array[len(array) - 1]
 min_num = array[0]
 
 for idx, i in enumerate(array):
